44a9917a8fba70514dd6546efbffa4a089a51957
grace butler <gmx.butler@gmail.com>
2021-05-23 16:01:12 -0500

    added the questions to use in to the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var score= 
+
 //count down timer start
 document.addEventListener('DOMContentLoaded',() =>{
 var timeLeftDisplay = document.querySelector("#secondsLeft")
@@ -17,3 +19,29 @@ function countDown(){
 startBtn.addEventListener("click", countDown)
 })
 //countdown timer end 
+
+//Questions start
+var questions =[
+    {
+        prompt: "A very usefull tool used during development and debugging for pirinting content to be debugger is: \n 1. Javascript \n 2. terminal/bash \n 3. for loops \n console.log", 
+        answer: "3. for loops"
+    }, 
+    {
+        prompt: "Commonly used data types DO NOT include: \n strings \n booleans \n alerts \n numbers",
+        answer: "alerts"
+    },
+    {
+        prompt: "Arrays in JavaScript can be used to store _______. \n numbers and strings \n other arrays \n booleans \n all of the above", 
+        answer: "all of the above"
+    },
+    {
+        prompt: "String values must be enclosed within_______ when being assigned to variables. \n commas \n curly brackets \n quotes \n parentheses",
+        answer: " quotes"
+    },
+    {
+        prompt: "The condition in an if/else statement is enclosed within __________. \n quotes \n curly brackets \n parentheses \n square brackets",
+        answer: "curly brackets"
+    }
+]
+
+//Questions end
