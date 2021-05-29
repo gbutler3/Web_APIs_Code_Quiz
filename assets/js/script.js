87d13f08955176887c1d35efd488a52ba8ef7cea
grace butler <gmx.butler@gmail.com>
2021-05-29 12:34:19 -0500

    changed when things are showing throughout the quiz ie the buttons and the instructions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,9 @@ var shuffledQuestions, currentQuestionIndex
 
 //count down timer start
 document.addEventListener('DOMContentLoaded',() =>{
-var timerEl = document.querySelector("#secondsLeft");
-var startbutton = document.querySelector("#startButton");
-var secondsLeft =questions.length *12 //TODO: change the time 
+var timerEl = document.getElementById("secondsLeft");
+var startbutton = document.getElementById("startButton");
+var secondsLeft =questions.length *12 //TODO: change the time  
 
 function startQuiz(){
     countDown(); 
@@ -16,6 +16,7 @@ function startQuiz(){
     shuffledQuestions= questions.sort(()=>Math.random( -.5 ));
     currentQuestionIndex= 0
     questionCard.classList.remove('hide');
+    instructionsCard.classList.add('hide')
     setQuestion();
 }
 
@@ -28,18 +29,15 @@ function countDown(){
         }
     }, 1000)
     }
-//when start button is clicked the timer starts the count down
 startbutton.addEventListener("click", startQuiz)
 })
-//countdown timer end 
-
 
 function setQuestion(){
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
-    questionElement.innerText =question.question
+    questionElement.innerText =question.question;
     
     question.answers.forEach(answer  => {    
         var button= document.createElement ('button')
@@ -53,19 +51,25 @@ function showQuestion(question) {
     }
 
 function checkQuestion (e){ //targets the event, but with different functionallity 
-    e.target.value; console.log(e.target.value)
+    // e.target.value; console.log(e.target.value)
     if (e.target.value){ //checking if it is true or false
-        console.log("correct");
+        console.log ("true")
     } else {
-        timerEl -= 5;//if the time is  1 it ends
+        console.log("false")
+        timerInterval -= 5;//if the time is  1 it ends
         }
-        console.log(timerEl)
         nextQuestion(); // pulls the new question ie takes the currentquestion and adds 1 to pull the next one
         clearQuestion(); //clears the list of answer options which are showing in the answer button options
         setQuestion(); //showQuestion(shuffledQuestions[currentQuestionIndex])
-}
+    }
 
-// function checkQuestion(e){}
+function nextQuestion () {
+    currentQuestionIndex ++ 
+    console.log(currentQuestionIndex)
+    if (shuffledQuestions[currentQuestionIndex] === question.length) {
+        endQuiz ();
+    } 
+}
 
 //variables used to end the game    
 var initialsEl = document.querySelector ("#name");
@@ -76,15 +80,51 @@ function endQuiz(){
     var scoresPageEl= document.getElementById("scoresPage");
     scoresPageEl.removeAttribute("class");
     questionCard.classList.add('hide');
+    secondsLeftCard.classList.add('hide');
+    backButton.classList.add('hide');
+    clearScore.classList.add('hide');
 }
 
-function nextQuestion () {
-    currentQuestionIndex ++ 
-    console.log(currentQuestionIndex)
-    if (shuffledQuestions[currentQuestionIndex] === question.length) {
-        endQuiz ();
-    } 
+var submitButton = document.getElementById("submitButton")
+submitButton.addEventListener("click", scoresPage) 
+    
+    //TODO: this needs work to show on HTML correctly 
+    var finalScoreEl= document.getElementById("finalScore");
+    finalScoreEl.textContent = secondsLeft; 
+
+
+//when the submit button is pressed the scores page is supposed to pull up 
+function scoresPage (){
+    submitButton.classList.add('hide');
+    highScores.classList.remove('hide');
+    questionCard.classList.add('hide');
+    backButton.classList.remove('hide');
+    clearScore.classList.remove('hide');
+    initials.classList.add('hide');
+    finalScore.classList.add('hide');
+    saveHighScore()
+    
 }
+
+function saveHighScore() {
+    var initials =initialsEl.value.trim();
+
+    if (initials !== " "){
+        var highscores= JSON.pars(window.localStorage.getItem("highscores")) || [];
+
+        var newScore= {
+            score: secondsLeft, 
+            initials: initials
+        };
+
+        highscores.push(newScore);
+        window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+        window.location.href= "score.html"
+        
+    }
+}
+
 
 function clearQuestion () {
     answerButtonElement.innerHTML=""
